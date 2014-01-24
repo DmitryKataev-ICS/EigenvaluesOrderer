db4cd2da51ca1f78aca71b3aee7e70b94064a61c
Dmitry Kataev <dekataev@gmail.com>
2014-01-24 14:55:50 +0400

    WTF!!1?

---
+++ EigenvaluesOrderer/BasicTypes.fs
@@ -0,0 +1,27 @@
+﻿namespace EigenvaluesOrderer
+
+type EigenValue(ReIm : float * float, V : float array * float array, refB : float * float, refC : float * float) =
+    static member closeEnough = 1.0E-10
+    member x.Re = fst(ReIm)
+    member x.Im = snd(ReIm)
+    member x.V = V
+    member x.refB = refB
+    member x.refC = refC
+    member x.isComplex = (abs x.Im) > EigenValue.closeEnough
+    static member isConjugate (a : EigenValue) (b : EigenValue) = a.Re = b.Re && (a.Im + b.Im < EigenValue.closeEnough)
+
+type Mode(a : EigenValue, b : EigenValue option) =
+    let _a = a
+    let _b = b
+    member x.isPair = _a.isComplex
+    member x.Re = _a.Re
+    member x.ImAbs = abs _a.Im
+    member x.V = Array.zip (fst _a.V) (snd _a.V)
+    member x.GetEV () = if _a.isComplex then [_a; _b.Value] else [_a]
+    member x.V_diff (a : Mode) = 
+            Array.map2 (fun (r1,i1) (r2,i2) -> (abs (r1-r2)) + (abs (i1-i2))) x.V a.V |> Array.sum
+    override x.Equals o = 
+        match o with
+            | :? Mode as b -> x.Re = b.Re && x.ImAbs = b.ImAbs
+            | _ -> false
+    override x.GetHashCode () = x.Re - x.ImAbs |> int
---
+++ EigenvaluesOrderer/ModesList.fs
@@ -0,0 +1,54 @@
+﻿namespace EigenvaluesOrderer
+
+type ModesList(modes : Mode list) =
+    let _all_modes = modes
+    member x.OscillatoryCount = (List.filter (fun (x : Mode) -> x.isPair) modes).Length
+    member x.AperiodicCount = _all_modes.Length - x.OscillatoryCount
+    member x.Count = x.OscillatoryCount + x.AperiodicCount
+    member x.Modes with get() = _all_modes
+    member x.ReorderWith (init : ModesList) =
+        if (x.AperiodicCount = init.AperiodicCount && x.OscillatoryCount = init.OscillatoryCount) then // this might be redundant
+            let pairs2swap =
+                [
+                    for i in 0..(x.Count - 1) ->
+                        (
+                            i,
+                            List.findIndex
+                                ((=) 
+                                    (List.minBy 
+                                        (fun a -> _all_modes.[i].V_diff a) 
+                                        init.Modes))
+                                init.Modes)]
+            ModesList([for i in 0..(x.Count - 1) -> _all_modes.[pairs2swap.[i] |> snd] ])
+        else
+            //match x-min oscillatory modes
+            let os2match_count = min x.OscillatoryCount init.OscillatoryCount
+            //match x-min aperiodic modes
+            let ap2match_count = min x.AperiodicCount init.AperiodicCount
+            //handle the reset
+    member x.unfold2primitives() =
+        let rec unfold2ev (eax : EigenValue list) (src : Mode list) =
+            if src.IsEmpty then
+                eax |> List.rev
+            else
+                unfold2ev ((src.Head.GetEV()) @ eax) src.Tail
+        let args =
+            _all_modes
+            |> unfold2ev []
+            |> List.fold
+                (
+                    fun (Vs, other) (ev : EigenValue) ->
+                        (
+                            [(fst ev.V) :: Vs.[0]; (snd ev.V) :: Vs.[1]],
+                            [
+                                ev.Re :: other.[0]; 
+                                ev.Im :: other.[1]; 
+                                (fst ev.refB) :: other.[2];
+                                (snd ev.refB) :: other.[3];
+                                (fst ev.refC) :: other.[4];
+                                (snd ev.refC) :: other.[5]] ))
+                ([ [[||]]; [[||]] ], [for i in 0..5 -> []])
+        let Vs = args |> fst |> List.map (List.rev>>List.toArray)
+        let other = args |> snd |> List.map (List.rev>>List.toArray)
+        (Vs, other)
+

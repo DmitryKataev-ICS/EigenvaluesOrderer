60772307204c13aefcaf86828f70508bee9de5fc
Dmitry Kataev <dekataev@gmail.com>
2014-02-19 00:11:48 +0400

    snd try

---
+++ EigenvaluesOrderer/ModesDictionary.fs
@@ -0,0 +1,38 @@
+﻿namespace EigenvaluesOrderer
+
+type TmpCell(fullkey : (float * float) array) =
+    member x.FullKey = fullkey
+    member x.IsEmpty = ref true
+    member x.EV : EigenValue option ref = ref None
+
+type EigenDict (keys : string list, keys_full : (float * float) array list, ev : EigenValue list) =
+    let fullkey_dist (a : (float * float) array) (b : (float * float) array) =
+        Array.map2 (fun (r1,i1) (r2,i2) -> (abs (r1-r2)) + (abs (i1-i2))) a b |> Array.sum
+    let rec min_index (cur : int*float) (src : (float * bool) list) i =
+        match src with
+            | head :: tail -> 
+                if (fst head) <= (snd cur) && (snd head) then 
+                    min_index (i, fst head) tail (i+1) 
+                else min_index cur tail (i+1)
+            | [] -> fst cur
+    let _dict =
+        if keys.IsEmpty then
+            List.zip (List.map (fun (a : EigenValue) -> a.getStringHash()) ev) ev
+            |> dict
+        else
+            //let wip = List.zip keys (List.map (fun a -> TmpCell(a)) keys_full) |> List.toSeq |> dict
+            let eax = List.map (fun a -> TmpCell(a)) keys_full
+            let all_distances = // all_distaces.[i].[j] :> i - eax index, j - ev index
+                List.map
+                    (fun (tmpcell : TmpCell) -> 
+                        List.map (fun (eig : EigenValue) -> fullkey_dist tmpcell.FullKey (Array.zip (fst eig.V) (snd eig.V))) ev)
+                    eax
+            List.iter
+                (
+                    fun (dist_vector : float list) -> // distances from current TmpCell to all subject eigenvalues
+                        let id = List.findIndex (fun (a : TmpCell) -> a.IsEmpty) eax
+                        let emptiness_map = List.map (fun (a : TmpCell) -> a.IsEmpty) eax
+                        let closest_id = min_index (id, eax.[id]) 
+                all_distances
+            
+    member x.EigenValues with get() = _dict
---
+++ EigenvaluesOrderer/Snapshot.fs
@@ -0,0 +1,35 @@
+﻿namespace EigenvaluesOrderer
+
+type Snaphot
+    (
+        reV : float array array,
+        imV : float array array,
+        reD : float array,
+        imD : float array,
+        reB : float array,
+        imB : float array,
+        reC : float array,
+        imC : float array,
+        states : string array) =
+    //let vdims = [reV.Length; reV.[0].Length; imV.Length; imV.[0].Length]
+    let _V = Array.zip reV imV
+    //let ddims = (reD.Length, imD.Length)
+    let _D = Array.zip reD imD
+    //let bdims = (reB.Length, imB.Length)
+    let _B = Array.zip reB imB
+    //let cdims = (reC.Length, imC.Length)
+    let _C = Array.zip reC imC
+    let _states = states
+    let _EV = 
+        let vdd = (_V.Length, _D.Length)
+        if not(fst vdd = snd vdd) then failwithf "V & D have different lengths: %i & %i" (fst vdd) (snd vdd)
+        let bcd = (_B.Length, _C.Length)
+        if not(fst bcd = snd bcd) then failwithf "B & C have different lengths: %i & %i" (fst bcd) (snd bcd)
+        if not(fst vdd = fst bcd) then failwithf "V (or D) & B (or C) have different lengths: %i & %i" (fst vdd) (fst bcd)
+        Array.map2
+            (fun (riv, rid) (rib, ric) -> EigenValue(rid, riv, rib, ric))
+            (Array.zip _V _D)
+            (Array.zip _B _C)
+        |> Array.toList
+
+
